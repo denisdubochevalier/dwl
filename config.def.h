41c1b03c72dd0315f54074a824fa4666a3a3b021
Denis Chevalier <contact@denischevalier.fr>
2024-04-12 03:26:05 +0200

    fix keyboard variant

--- config.def.h
+++ config.def.h
@@ -150,7 +150,7 @@ static const struct xkb_rule_names xkb_rules = {
     /* example:
     .options = "ctrl:nocaps",
     */
-    .variant = "intl",
+    .variant = ",intl",
     .options = NULL,
 };
 
